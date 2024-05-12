b4dd7398fdc9365948437ab3e66d39ceaade5058
tgorsk15 <tgbusiness463@gmail.com>
2024-05-12 14:18:07 -0500

    Style info display section
    Also made some minor page structure adjustments

--- src/components/infoDisplay.jsx
+++ src/components/infoDisplay.jsx
@@ -5,11 +5,26 @@ export function InfoDisplay({ infoDetails }) {
 
     return (
         <div className="info-display-container">
-            <h2>{infoDetails.fullName}</h2>
+            <h2 className="info-title">{infoDetails.fullName}</h2>
             <div className="info-details">
-                <h4>{infoDetails.email}</h4>
-                <h4>{infoDetails.phone}</h4>
-                <h4>{infoDetails.location}</h4>
+                
+                <h4>
+                    <label htmlFor="">Email:</label>
+                    {infoDetails.email}
+                </h4>
+                <h4>
+                    <label htmlFor="">Phone:</label>
+                    {infoDetails.phone}
+                </h4>
+                <h4>
+                    <label htmlFor="">Location:</label>
+                    {infoDetails.location}
+                </h4>
+            </div>
+            <div className="social-container">
+                <p className="linked-in"><i className="devicon-linkedin-plain"></i> Jack Sparrow</p>
+                <p className="git-hub"><i className="devicon-github-original"></i> wallaby4040</p>
+                <p></p>
             </div>
         </div>
     )
