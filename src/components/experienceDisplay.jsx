76f0b5b82101ac3165ddfd400c662babcc9fd019
tgorsk15 <tgbusiness463@gmail.com>
2024-05-12 12:50:36 -0500

    style Experience Display section

--- src/components/experienceDisplay.jsx
+++ src/components/experienceDisplay.jsx
@@ -3,16 +3,20 @@ export function ExperienceDisplay({experienceData}) {
 
     return (
         <div className="experience-display-container">
-            <h2 className="experience-header-display">Experience</h2>
+            <h2 className="experience-title">Experience</h2>
 
             <div className="experience-details">
                 {experienceData.map((experience) => {
                     return (
                         <div key={experience.id} className="experience-display">
-                            <h4>{experience.employer}</h4>
-                            <h4>{experience.role}</h4>
-                            <h4>{experience.startDate}</h4>
-                            <h4>{experience.endDate}</h4>
+                            {/* style the below:*/}
+                            <p className="employer-info-display">
+                                <span className="employer-display">{experience.employer}</span> - <span className="role-display">{experience.role}</span>
+                            </p>
+                            <p className="employer-dates-display">
+                                {experience.startDate} - {experience.endDate}
+                            </p>
+                            
                         </div>
                     )
                 })}
